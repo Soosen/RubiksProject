167d843d95e977be4f322bb8ba8c5a8f6f45eef2
Soosen <mateusz.sosnowski.zg@gmail.com>
2023-11-10 22:03:10 +0100

    changed textues path

--- src/utilities.js
+++ src/utilities.js
@@ -218,23 +218,23 @@ class Utilities{
         var path = "";
         switch(tpID){
             case 0:
-                path += "/textures/used";
+                path += "../textures/used";
                 break;
 
             case 1:
-                path += "/textures/veryused";
+                path += "../textures/veryused";
                 break;
 
             case 2:
-                path += "/textures/scratched";
+                path += "../textures/scratched";
                 break;
 
             case 3:
-                path += "/textures/dots";
+                path += "../textures/dots";
                 break;
 
             case 4:
-                path += "/textures/basic";
+                path += "../textures/basic";
                 break;
         }
            
